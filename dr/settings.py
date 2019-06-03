8e35f72dbe0201ddfaaaedadc8ef3cea418db572
Anton <pozernishku@gmail.com>
2019-06-03 11:40:32 +0300

    sting <br> is added as breakliner.

--- dr/settings.py
+++ dr/settings.py
@@ -16,10 +16,10 @@ NEWSPIDER_MODULE = 'dr.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'dr (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
